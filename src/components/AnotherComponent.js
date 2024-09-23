c8d93421817e77db7444cb9369fc64f71a888fb3
Elisei-Trifan <slavaberzan@gmail.com>
2024-09-23 17:01:59 +0300

    change file

---
+++ src/components/AnotherComponent.js
@@ -0,0 +1,11 @@
+import './AnotherComponent.css'
+
+function AnotherComponent() {
+  return (
+    <ldiv>
+      <h2 className="component">Hello from homework</h2>
+    </ldiv>
+  )
+}
+
+export default AnotherComponent
